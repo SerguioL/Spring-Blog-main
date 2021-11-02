cc8353d20868863d83a10ab9241ab2eae9298ecc
SerguioL <serguio.l.landa@gmail.com>
2021-11-02 10:10:03 -0500

    first commit

---
+++ src/main/java/com/example/springblogmain/SpringBlogMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.springblogmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springblogmain/SpringBlogMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springblogmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
